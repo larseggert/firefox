5516f6d84fa33c7f8a9fef5fa324dbc82b30ac3f
Michael Froman <mfroman@mozilla.com>
2026-08-21 12:38:47 -0500

    Bug 2064200 - Vendor libwebrtc from fd3145f900
    Upstream commit: https://webrtc.googlesource.com/src/+/fd3145f9004c06745633d06d267b8887badff0e2
        Pass VideoReceiveStreamInterface::Config by value
    
        Changes VideoReceiveStreamInterface::Config to be passed by value
        instead of const reference.
    
        Passing by value was already being done in most cases so this change is
        more about aligning the video and audio structs in terms of enforcing
        move semantics. Two test frameworks are updated to use `std::move` in
        this CL rather than creating copies.
    
        Bug: none
        Change-Id: Icb94a04c928b23136f1695c74038767ea6112197
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/493420
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#48291}

--- third_party/libwebrtc/call/video_receive_stream.h
+++ third_party/libwebrtc/call/video_receive_stream.h
@@ -20,7 +20,6 @@
 #include <string>
 #include <utility>
 #include <vector>
-
 #include "api/call/transport.h"
 #include "modules/rtp_rtcp/include/rtp_rtcp_defines.h"
 #include "api/crypto/crypto_options.h"
@@ -202,22 +201,19 @@ class VideoReceiveStreamInterface : public MediaReceiveStreamInterface {
   };
 
   struct Config {
-   private:
-    // Access to the copy constructor is private to force use of the Copy()
-    // method for those exceptional cases where we do use it.
-    Config(const Config&);
-
    public:
     Config() = delete;
+    Config(const Config&) = delete;
+    Config& operator=(const Config&) = delete;
     Config(Config&&);
     Config(Transport* rtcp_send_transport,
            VideoDecoderFactory* decoder_factory = nullptr);
     Config& operator=(Config&&);
-    Config& operator=(const Config&) = delete;
     ~Config();
 
-    // Mostly used by tests.  Avoid creating copies if you can.
-    Config Copy() const { return Config(*this); }
+    // Mostly used by tests. Avoid creating copies if you can.
+    // Note that this method will not copy move-only fields.
+    Config Copy() const;
 
     std::string ToString() const;
 
