5e10c8e55afdb00e1561894563156442bed9999a
Michael Froman <mfroman@mozilla.com>
2026-08-24 18:58:28 -0500

    Bug 2064200 (MOZ) - upstream added a method to VideoReceiveStreamInterface

--- media/webrtc/signaling/gtest/MockCall.h
+++ media/webrtc/signaling/gtest/MockCall.h
@@ -205,6 +205,8 @@ class MockVideoReceiveStream : public webrtc::VideoReceiveStreamInterface {
   virtual void SetAssociatedPayloadTypes(
       std::map<int, int> associated_payload_types) override {}
 
+  virtual void SetRawPayloadTypes(std::set<int> raw_payload_types) override {}
+
   virtual void UpdateRtxSsrc(uint32_t ssrc) override {};
 
   virtual ~MockVideoReceiveStream() {}
