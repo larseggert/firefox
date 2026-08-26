6a22871ac112bd49bf6f0818cef6788c3ed7cc4b
Dão Gottwald <dao@mozilla.com>
2026-08-26 14:18:50 +0000

    Bug 2065803 - Record the surface multi-selected tabs were pinned from. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D320685

--- browser/components/tabbrowser/Tabbrowser.sys.mjs
+++ browser/components/tabbrowser/Tabbrowser.sys.mjs
@@ -8878,7 +8878,9 @@ export class Tabbrowser {
       tabCount: tabs.length,
     });
     for (let tab of tabs) {
-      this.pinTab(tab, this.TabMetrics.decomposedContext(metricsContext));
+      this.pinTab(tab, {
+        metricsContext: this.TabMetrics.decomposedContext(metricsContext),
+      });
     }
   }
 
