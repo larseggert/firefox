c7572d318c0dcb13e84791c12b2b4374f4f4f75e
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-08-26 14:01:55 +0000

    Bug 2066184 - Part 3: Navigate to Notification or action's navigation URL on click. r=saschanaz
    Differential Revision: https://phabricator.services.mozilla.com/D320995

--- dom/notification/NotificationParent.cpp
+++ dom/notification/NotificationParent.cpp
@@ -98,11 +98,23 @@ class NotificationObserver final : public nsIAlertCallbacks {
             .action = Some(aAction ? "action-button"_ns : "body"_ns),
             .siteCategory = mCategory}));
 
-    if (RunActor([](auto* actor) { actor->FireClickEvent(); })) {
-      return NS_OK;
-    } else if (mScope.IsEmpty()) {
-      // No actor there, we need to open up a window ourselves
-      return OpenWindowFor(mPrincipal);
+    nsCOMPtr<nsIURI> navigate;
+    if (StaticPrefs::dom_webnotifications_navigate_enabled()) {
+      if (aAction) {
+        aAction->GetNavigate(getter_AddRefs(navigate));
+      } else {
+        navigate = mNotification.options().navigate();
+      }
+    }
+
+    // If navigation URL is set, we will navigate in RespondOnClick.
+    if (!navigate) {
+      if (RunActor([](auto* actor) { actor->FireClickEvent(); })) {
+        return NS_OK;
+      } else if (mScope.IsEmpty()) {
+        // No actor there, we need to open up a window ourselves
+        return OpenWindowFor(mPrincipal);
+      }
     }
 
     nsAutoString actionName;
