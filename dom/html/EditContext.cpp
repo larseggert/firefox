980e89fdcb3cfe520f7bace6ec4db17f4b47c997
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-08-31 13:03:21 +0000

    Bug 2067230 - Set white-space:pre on EditContext anonymous element. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D321957

--- dom/html/EditContext.cpp
+++ dom/html/EditContext.cpp
@@ -172,12 +172,15 @@ EditContext::EditContext(nsIGlobalObject* aGlobalObject,
   MOZ_ASSERT(mTextContainer);
   mText = document->CreateTextNode(u""_ns);
   mText->MarkAsMaybeModifiedFrequently();
-  mTextContainer->AppendChild(*mText, IgnoreErrors());
-  anonymousContent->Root()->AppendChild(*mTextContainer, IgnoreErrors());
+  mTextContainer->AppendChild(*mText, aRv);
+  anonymousContent->Root()->AppendChild(*mTextContainer, aRv);
   mText->SetEditableFlag(true);
   mTextContainer->SetEditableFlag(true);
   mTextContainer->Style()->SetProperty("visibility"_ns, "hidden"_ns, ""_ns,
-                                       IgnoreErrors());
+                                       aRv);
+  // For computing target ranges for backspace, etc.,
+  // we shouldn't treat consecutive spaces as collapsed.
+  mTextContainer->Style()->SetProperty("white-space"_ns, "pre"_ns, ""_ns, aRv);
   mSelectionStart = aInit.mSelectionStart;
   mSelectionEnd = aInit.mSelectionEnd;
   UpdateTextInternal(0, 0, aInit.mText, aRv);
