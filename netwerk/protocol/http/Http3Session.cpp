0ea3bb6c3d88eb1fdfbc3c80fc08672f50e7f582
Anna Weine <anna.weine@mozilla.com>
2026-09-09 14:56:01 +0000

    Bug 2066838 - Add telemetry for the resumption outcome r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D321573

--- netwerk/protocol/http/Http3Session.cpp
+++ netwerk/protocol/http/Http3Session.cpp
@@ -241,10 +241,13 @@ nsresult Http3Session::Init(const nsHttpConnectionInfo* aConnInfo,
   if (StaticPrefs::network_http_http3_enable_0rtt() && !hasServCertHashes()) {
     uint32_t maxAttempts =
         StaticPrefs::network_ssl_tokens_cache_records_per_entry();
+    bool tokenFound = false;
+    bool tokenAccepted = false;
     for (uint32_t attempt = 0; attempt < maxAttempts; ++attempt) {
       if (NS_FAILED(SSLTokensCache::Get(peerId, token, info))) {
         break;
       }
+      tokenFound = true;
       LOG(("Found a resumption token in the cache [attempt=%u].", attempt));
       nsresult rv = mHttp3Connection->SetResumptionToken(token);
       if (NS_FAILED(rv)) {
@@ -252,6 +255,7 @@ nsresult Http3Session::Init(const nsHttpConnectionInfo* aConnInfo,
              attempt));
         continue;
       }
+      tokenAccepted = true;
       mSocketControl->SetSessionCacheInfo(std::move(info));
       if (mHttp3Connection->IsZeroRtt()) {
         LOG(("Can send ZeroRtt data"));
@@ -278,6 +282,9 @@ nsresult Http3Session::Init(const nsHttpConnectionInfo* aConnInfo,
         NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Dispatch failed");
       }
       break;
+    }
+    if (tokenFound && !tokenAccepted) {
+      glean::network::ssl_token_resumption_outcome.Get("rejected"_ns).Add();
     }
   }
 
@@ -2802,6 +2809,15 @@ void Http3Session::SetSecInfo() {
     mSocketControl->SetInfo(secInfo.cipher, secInfo.version, secInfo.group,
                             secInfo.signature_scheme, secInfo.ech_accepted);
     mHandshakeSucceeded = true;
+
+    bool tokenPresent = false;
+    if (NS_SUCCEEDED(
+            mSocketControl->GetResumptionTokenPresent(&tokenPresent)) &&
+        tokenPresent) {
+      glean::network::ssl_token_resumption_outcome
+          .Get(secInfo.resumed ? "resumed"_ns : "not_resumed"_ns)
+          .Add();
+    }
   }
 
   if (!mSocketControl->HasServerCert()) {
