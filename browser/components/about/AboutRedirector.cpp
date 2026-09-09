09e8eecb74e1b68fb4dd6d5849b1827695750608
Jared Hirsch <ohai@6a68.net>
2026-09-09 17:56:39 +0000

    Bug 2063777 - Hide about:referrals from autocomplete and about:about r=niklas
    Differential Revision: https://phabricator.services.mozilla.com/D324416

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -126,7 +126,8 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
     {"referrals", "https://www.firefox.com/invite/",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
-         nsIAboutModule::URI_MUST_LOAD_IN_CHILD},
+         nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
+         nsIAboutModule::HIDE_FROM_ABOUTABOUT},
     {"restartrequired", "chrome://browser/content/aboutRestartRequired.xhtml",
      nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::HIDE_FROM_ABOUTABOUT},
     {"rights", "https://www.mozilla.org/about/legal/terms/firefox/",
