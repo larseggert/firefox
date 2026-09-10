4129f25d1208b355ccac291f362b5ea98ced738f
Dustin Whisman <dwhisman@mozilla.com>
2026-09-10 16:26:28 +0000

    Bug 2037111 - Rename tab tokens for naming consistency r=desktop-theme-reviewers,tabbrowser-reviewers,jswinarton,tgiles,kcochrane
    Differential Revision: https://phabricator.services.mozilla.com/D321985

--- tools/lint/stylelint/stylelint-plugin-mozilla/config.mjs
+++ tools/lint/stylelint/stylelint-plugin-mozilla/config.mjs
@@ -152,7 +152,7 @@ const BackgroundColor = {
     "--tab-group-background-color",
     "--tab-group-text-color",
     "--tab-group-background-color-hover",
-    "--tab-loading-fill",
+    "--tab-icon-fill-loading",
     "--tabgroup-swatch-color-invert",
     "--tabgroup-swatch-color",
     "--toolbar-background-color",
