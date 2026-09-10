8a88bbc810e4a662f96ad741204a6337f4938b14
Rong Bao <webmaster@csmantle.top>
2026-09-10 14:21:33 +0000

    Bug 2070770 - [loong64] Part 1: Optimize zero immediate cases in ma_add_d(Register, Register, Imm{32,Word}). r=jandem
    This simplifies the implementation of the Imm32 variant with the new
    ma_move, then add special-casing for ImmWord variant.
    
    Differential Revision: https://phabricator.services.mozilla.com/D324822

--- js/src/jit/loong64/MacroAssembler-loong64.cpp
+++ js/src/jit/loong64/MacroAssembler-loong64.cpp
@@ -356,11 +356,9 @@ FaultingCodeRange MacroAssemblerLOONG64::ma_st_d(Register src,
 
 // Add.
 void MacroAssemblerLOONG64::ma_add_d(Register rd, Register rj, Imm32 imm) {
-  if (rd == rj && imm.value == 0) {
-    // This is nop.
-    return;
-  }
-  if (is_intN(imm.value, 12)) {
+  if (imm.value == 0) {
+    ma_move(rd, rj);
+  } else if (is_intN(imm.value, 12)) {
     as_addi_d(rd, rj, imm.value);
   } else if (rd != rj) {
     ma_li(rd, imm);
@@ -375,7 +373,9 @@ void MacroAssemblerLOONG64::ma_add_d(Register rd, Register rj, Imm32 imm) {
 }
 
 void MacroAssemblerLOONG64::ma_add_d(Register rd, Register rj, ImmWord imm) {
-  if (is_intN(imm.value, 12)) {
+  if (imm.value == 0) {
+    ma_move(rd, rj);
+  } else if (is_intN(imm.value, 12)) {
     as_addi_d(rd, rj, imm.value);
   } else if (rd != rj) {
     ma_li(rd, imm);
