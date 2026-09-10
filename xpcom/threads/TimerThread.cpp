88e91522aa4e2ca1a0094055a1d789db16d71b97
agoloman <agoloman@mozilla.com>
2026-09-10 12:09:18 +0300

    Revert "Bug 2070537 - Restore StoreName on marker types that lost it when they were migrated to BaseMarkerType. r=canova" for causing build bustages @Logging.cpp.
    This reverts commit 4400b8967ba3a30a753d10c7234c3a76ba760082.

--- xpcom/threads/TimerThread.cpp
+++ xpcom/threads/TimerThread.cpp
@@ -320,8 +320,6 @@ void TimerEventAllocator::Free(void* aPtr) {
 
 struct TimerMarker : public BaseMarkerType<TimerMarker> {
   static constexpr const char* Name = "Timer";
-  // Call sites pass the timer's own name, so ETW must keep that name.
-  static constexpr bool StoreName = true;
   using MS = MarkerSchema;
   static constexpr MS::PayloadField PayloadFields[] = {
       {"delay", MS::InputType::TimeDuration, "Delay", MS::Format::Milliseconds},
@@ -396,9 +394,6 @@ struct TimerMarker : public BaseMarkerType<TimerMarker> {
 
 struct AddRemoveTimerMarker : public BaseMarkerType<AddRemoveTimerMarker> {
   static constexpr const char* Name = "AddRemoveTimer";
-  // Call sites pass "PostTimerEvent", "AddTimer" or "RemoveTimer", so ETW must
-  // keep that name.
-  static constexpr bool StoreName = true;
   using MS = MarkerSchema;
   static constexpr MS::PayloadField PayloadFields[] = {
       {"name", MS::InputType::CString, "Name", MS::Format::String},
