e33f4ef93182255c95473b016293cc9d8649aa7b
Jan Varga <jvarga@igalia.com>
2026-09-10 04:33:47 +0000

    Bug 2057579 - Complete the CSSMatrixComponent and CSSTransformComponent implementations; r=emilio,firefox-style-system-reviewers
    Complete the remaining `CSSMatrixComponent` functionality by implementing the
    missing `matrix` setter and `ToMatrix()`, including the required 2D flattening.
    
    This completes `CSSTransformComponent::ToMatrix()` dispatch and enables the
    remaining WPT coverage for transform component objects.
    
    Differential Revision: https://phabricator.services.mozilla.com/D314899

--- layout/style/typedom/CSSTransformComponent.cpp
+++ layout/style/typedom/CSSTransformComponent.cpp
@@ -100,10 +100,11 @@ already_AddRefed<DOMMatrix> CSSTransformComponent::ToMatrix(ErrorResult& aRv) {
         return GetAsCSSPerspective().ToMatrix(aRv);
       }
 
-      default:
-        aRv.Throw(NS_ERROR_NOT_IMPLEMENTED);
-        return nullptr;
+      case TransformComponentType::MatrixComponent: {
+        return GetAsCSSMatrixComponent().ToMatrix(aRv);
+      }
     }
+    MOZ_MAKE_COMPILER_ASSUME_IS_UNREACHABLE("Bad type value!");
   }(aRv);
 
   if (aRv.Failed()) {
